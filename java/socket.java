8b56399c61e1ac2454952e1c7f08ce7790b1d98d
Raphael Alonso de Mendonça <raphamendonca@users.noreply.github.com>
2017-03-29 10:59:30 -0300

    Update socket.java
    Adição do código desenvolvido no eclispe.

--- java/socket.java
+++ java/socket.java
@@ -1,1 +1,94 @@
-dasdsa
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class SocketUtil {
+	Socket socket;
+
+	private String HOSTNAME;
+	private int PORTNUMBER;
+
+	public SocketUtil( String hostName, int portNumber) {
+		HOSTNAME = hostName;
+		PORTNUMBER = portNumber;
+
+	}
+	
+	private void socketConnect(String host, int port) {
+		try {
+			socket = new Socket(host, port);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void socketClose(){
+		if (socket != null) {
+			try {
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			socket = null;
+		}
+	}
+
+	public String executaBusca(String commando, String finalMarker){
+
+		socketConnect(HOSTNAME, PORTNUMBER);
+		
+		int c;
+		
+		InputStream in = null;
+		try {
+			in = socket.getInputStream();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		OutputStream out = null;
+		try {
+			out = socket.getOutputStream();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		byte buf[] = commando.getBytes();
+		
+		try {
+			out.write(buf);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		try {
+			in.read();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		StringBuffer result = new StringBuffer();
+
+		boolean isHeader = true;
+
+		char caractere;
+		try {
+			while ((c = in.read()) != -1) {
+				caractere = ((char) c);
+				result.append(caractere);
+
+				if (result.toString().contains(finalMarker)) {
+					break;
+				}
+			
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		socketClose();
+		
+		return result.toString();
+	}
+}
