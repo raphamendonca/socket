d87b43e42e89a52ce50279375e096026cb17b759
Raphael Alonso de Mendonça <raphamendonca@users.noreply.github.com>
2017-03-29 11:05:30 -0300

    Adição do código de socket via nodejs

--- node/socket.js
+++ node/socket.js
@@ -1,1 +1,31 @@
-/* Exemplo de socket com nodejs */
+/* Exemplo de socket com nodejs
+Exemplo obtido na internet, assim que recuperar o site eu adiciono aqui a referencia
+*/
+
+var net = require('net');
+
+var hostname = '127.0.0.1';
+var portNumber = 1234;
+var comando = 'OLá';
+
+var client = new net.Socket();
+client.connect(portNumber, hostname, function() {
+	console.log('Connected');
+	
+});
+
+var i = 0;
+client.on('data', function(data) {
+	client.write(comando);
+	console.log('Received: ' + data);
+	
+	i++;
+	if(i===2){
+		console.log('----');
+		client.destroy(); 
+	}
+});
+
+client.on('close', function() {
+	console.log('Connection closed');
+});
